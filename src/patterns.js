829743da3c7945685988db3612d3566034c70ba0
Li Kai <hello+github@lik.ai>
2019-01-19 17:59:08 +0800

    fix: normalize all new lines

---
+++ src/patterns.js
@@ -0,0 +1,18 @@
+const pt = {
+  TYPE: /(?<type>[a-zA-Z]+)/,
+  SCOPE: /\((?<scope>[\w-]+)\)/,
+  OPTIONAL_SCOPE: /(?:\((?<scope>[\w-]+)?\))?/,
+  DESCRIPTION: /(?<description>\w.+)/,
+  BREAKING_CHANGE: /(?<breakingChange>BREAKING CHANGE)/,
+  CONTENT: /(?<content>(?:[^\n]+\n?)+)/,
+};
+
+pt.HEADER = new RegExp(
+  `^${pt.TYPE.source}${pt.OPTIONAL_SCOPE.source}: ${pt.DESCRIPTION.source}$`
+);
+
+pt.BODY = new RegExp(
+  `^(?:${pt.BREAKING_CHANGE.source}: )?${pt.CONTENT.source}`
+);
+
+module.exports = pt;

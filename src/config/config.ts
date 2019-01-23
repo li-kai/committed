44b7302131f64208ecb36b92cbc03f4ebf9e17bf
Li Kai <hello+github@lik.ai>
2019-01-23 21:03:56 +0800

    fix: getConfig throws error
    if getConfig isn't a module not found error, rethrow the error

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,18 @@
 import path from 'path';
+import defaultConfig from './defaultConfig';
 
-function readConfig(rootPath: string) {
-  return import(path.join(rootPath, 'committed.config.js')).catch((error) => {
-    console.error(error);
-  });
+/**
+ * Merges config with the default config if available, else, uses default config.
+ * @param rootPath rootPath of folder to find config
+ */
+async function getConfig(rootPath: string) {
+  try {
+    const config = await import(path.join(rootPath, 'committed.config.js'));
+    return { ...defaultConfig, config };
+  } catch (error) {
+    if (error.code !== 'MODULE_NOT_FOUND') throw error;
+  }
+  return defaultConfig;
 }
 
-export default readConfig;
+export default getConfig;
